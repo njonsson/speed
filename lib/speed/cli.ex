d00d80ed703d016ec89c315bd9fd46a9d18cb6d6
Nils Jonsson <nils@alumni.rice.edu>
2017-06-22 17:58:09 -0500

    Add a typespec to CLI.run/1 and return zero

--- lib/speed/cli.ex
+++ lib/speed/cli.ex
@@ -6,8 +6,10 @@ defmodule Speed.CLI do
   @doc """
   The main entry point for the escript.
   """
+  @spec main([binary]) :: non_neg_integer
   def main(_arguments) do
     IO.puts "WELCOME TO SPEED!"
     IO.puts "THANKS FOR PLAYING."
+    0
   end
 end
