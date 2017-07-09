eacbd2e89e0499806beecfcec1c9f2e6128c49df
Nils Jonsson <nils@alumni.rice.edu>
2017-07-08 21:00:01 -0500

    Implement Deck.deal/1

---
+++ lib/speed.ex
@@ -0,0 +1,8 @@
+defmodule Speed do
+  @moduledoc """
+  Contains an implementation of the Speed card game.
+  """
+
+  @typedoc "An error result"
+  @type error :: {:error, binary}
+end
--- lib/speed/deck.ex
+++ lib/speed/deck.ex
@@ -4,10 +4,55 @@ defmodule Speed.Deck do
   """
 
   @typedoc "A list of binaries representing playing cards."
-  @type t :: [binary]
+  @type t :: [card]
+
+  @typedoc "A binary representing a playing card."
+  @type card :: binary
+
+  @typedoc "The successful result of dealing to a player"
+  @type dealt :: {list(card), list(card), card}
 
   @suits ~w( ♠ ♣ ♥ ♦ )
   @numbers ~w( A 2 3 4 5 6 7 8 9 10 J Q K )
+
+  @doc """
+  Deals the specified `deck` to two players, returning those players’ hands, draw
+  piles and face-down cards, plus the remaining deck.
+
+  ## Examples
+
+      iex> ["A♠", "2♠", "3♠"] |> Speed.Deck.deal
+      {:error, "Not enough cards in the deck"}
+
+      iex> shuffled_deck = Speed.Deck.new |> Speed.Deck.shuffle
+      ...> {{[_, _, _, _, _], [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _], _}, {[_, _, _, _, _], [_, _, _, _, _, _, _, _, _, _, _, _, _, _, _], _}, remaining_deck} = shuffled_deck |> Speed.Deck.deal
+      ...> remaining_deck |> length
+      10
+  """
+  @spec deal(t) :: {{dealt, dealt}, t} | Speed.error
+  def deal(deck) do
+    if length(deck) < 40 do
+      {:error, "Not enough cards in the deck"}
+    else
+      hands_cards = deck |> Enum.take(10)
+      hand1 = hands_cards |> Enum.take_every(2)
+      hand2 = hands_cards |> Enum.drop(1) |> Enum.take_every(2)
+
+      draw_piles_cards = deck |> Enum.drop(10) |> Enum.take(30)
+      draw_pile1 = draw_piles_cards |> Enum.take_every(2)
+      draw_pile2 = draw_piles_cards |> Enum.drop(1) |> Enum.take_every(2)
+
+      face_down_cards = deck |> Enum.drop(40) |> Enum.take(2)
+      face_down_card1 = face_down_cards |> Enum.at(0)
+      face_down_card2 = face_down_cards |> Enum.at(1)
+
+      remaining_deck = deck |> Enum.drop(42)
+
+      {{hand1, draw_pile1, face_down_card1},
+       {hand2, draw_pile2, face_down_card2},
+       remaining_deck}
+    end
+  end
 
   @doc """
   Constructs a deck using the specified `options`.
